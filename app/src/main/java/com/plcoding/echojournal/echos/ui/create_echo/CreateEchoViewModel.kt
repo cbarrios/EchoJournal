25e2b03680ae80595aa8fa4e8353b8b7fa7737e1
Carlos Barrios <lalobarrios16@yahoo.com>
2025-05-27 16:49:01 -0400

    (25) 6.4 Confirm leave dialog

--- app/src/main/java/com/plcoding/echojournal/echos/ui/create_echo/CreateEchoViewModel.kt
+++ app/src/main/java/com/plcoding/echojournal/echos/ui/create_echo/CreateEchoViewModel.kt
@@ -39,12 +39,10 @@ class CreateEchoViewModel : ViewModel() {
     fun onAction(action: CreateEchoAction) {
         when (action) {
             is CreateEchoAction.OnAddTopicTextChange -> onAddTopicTextChange(action.text)
-            CreateEchoAction.OnCancelClick -> TODO()
             CreateEchoAction.OnConfirmMood -> onConfirmMood()
             CreateEchoAction.OnDismissMoodSelector -> onDismissMoodSelector()
             CreateEchoAction.OnDismissTopicSuggestions -> onDismissTopicSuggestions()
             is CreateEchoAction.OnMoodClick -> onMoodClick(action.moodUi)
-            CreateEchoAction.OnNavigateBackClick -> TODO()
             is CreateEchoAction.OnNoteTextChange -> TODO()
             CreateEchoAction.OnPauseAudioClick -> TODO()
             CreateEchoAction.OnPlayAudioClick -> TODO()
@@ -54,6 +52,10 @@ class CreateEchoViewModel : ViewModel() {
             is CreateEchoAction.OnTopicClick -> onTopicClick(action.topic)
             is CreateEchoAction.OnTrackSizeAvailable -> TODO()
             CreateEchoAction.OnSelectMoodClick -> onSelectMoodClick()
+            CreateEchoAction.OnDismissConfirmLeaveDialog -> onDismissConfirmLeaveDialog()
+            CreateEchoAction.OnCancelClick,
+            CreateEchoAction.OnNavigateBackClick,
+            CreateEchoAction.OnGoBack -> onShowConfirmLeaveDialog()
         }
     }
 
@@ -74,6 +76,18 @@ class CreateEchoViewModel : ViewModel() {
                 }
             }
             .launchIn(viewModelScope)
+    }
+
+    private fun onShowConfirmLeaveDialog() {
+        _state.update {
+            it.copy(showConfirmLeaveDialog = true)
+        }
+    }
+
+    private fun onDismissConfirmLeaveDialog() {
+        _state.update {
+            it.copy(showConfirmLeaveDialog = false)
+        }
     }
 
     private fun onDismissTopicSuggestions() {
