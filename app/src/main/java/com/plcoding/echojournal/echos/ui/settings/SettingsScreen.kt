beca1a4bbef26c5a647251d27cf7f89b6130039b
Carlos Barrios <lalobarrios16@yahoo.com>
2025-06-02 20:08:30 -0400

    (38) 8.4 Changing the default topics

--- app/src/main/java/com/plcoding/echojournal/echos/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/plcoding/echojournal/echos/ui/settings/SettingsScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -29,7 +30,9 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.plcoding.echojournal.R
 import com.plcoding.echojournal.core.ui.design.theme.EchoJournalTheme
 import com.plcoding.echojournal.core.ui.design.theme.bgGradient
+import com.plcoding.echojournal.core.ui.util.defaultShadow
 import com.plcoding.echojournal.echos.ui.models.MoodUi
+import com.plcoding.echojournal.echos.ui.settings.components.DefaultTopicSelectorCard
 import com.plcoding.echojournal.echos.ui.settings.components.MoodCard
 import org.koin.androidx.compose.koinViewModel
 
@@ -99,7 +102,34 @@ fun SettingsScreen(
                 selectedMood = state.selectedMood,
                 onMoodClick = {
                     onAction(SettingsAction.OnMoodClick(it))
-                }
+                },
+                modifier = Modifier
+                    .defaultShadow(shape = RoundedCornerShape(8.dp))
+            )
+            DefaultTopicSelectorCard(
+                topics = state.topics,
+                searchText = state.searchText,
+                topicSuggestions = state.suggestedTopics,
+                showCreateTopicOption = state.showCreateTopicOption,
+                showSuggestionsDropDown = state.isTopicSuggestionsVisible,
+                canInputText = state.isTopicTextInputVisible,
+                onSearchTextChange = {
+                    onAction(SettingsAction.OnSearchTextChange(it))
+                },
+                onToggleCanInputText = {
+                    onAction(SettingsAction.OnAddButtonClick)
+                },
+                onAddTopicClick = {
+                    onAction(SettingsAction.OnSelectTopicClick(it))
+                },
+                onRemoveTopicClick = {
+                    onAction(SettingsAction.OnRemoveTopicClick(it))
+                },
+                onDismissSuggestionsDropDown = {
+                    onAction(SettingsAction.OnDismissTopicDropDown)
+                },
+                modifier = Modifier
+                    .defaultShadow(shape = RoundedCornerShape(8.dp))
             )
         }
     }
--- app/src/main/java/com/plcoding/echojournal/echos/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/plcoding/echojournal/echos/ui/settings/SettingsViewModel.kt
@@ -1,15 +1,27 @@
+@file:OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
+
 package com.plcoding.echojournal.echos.ui.settings
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.plcoding.echojournal.echos.domain.echo.EchoDataSource
 import com.plcoding.echojournal.echos.domain.echo.Mood
 import com.plcoding.echojournal.echos.domain.settings.SettingsPreferences
 import com.plcoding.echojournal.echos.ui.models.MoodUi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChangedBy
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
@@ -17,7 +29,8 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class SettingsViewModel(
-    private val settingsPreferences: SettingsPreferences
+    private val settingsPreferences: SettingsPreferences,
+    private val echoDataSource: EchoDataSource
 ) : ViewModel() {
 
     private var hasLoadedInitialData = false
@@ -27,6 +40,7 @@ class SettingsViewModel(
         .onStart {
             if (!hasLoadedInitialData) {
                 observeSettings()
+                observeTopicSearchResults()
                 hasLoadedInitialData = true
             }
         }
@@ -38,13 +52,65 @@ class SettingsViewModel(
 
     fun onAction(action: SettingsAction) {
         when (action) {
-            SettingsAction.OnAddButtonClick -> {}
+            SettingsAction.OnAddButtonClick -> onAddButtonClick()
             SettingsAction.OnBackClick -> {}
             is SettingsAction.OnSelectTopicClick -> onSelectTopicClick(action.topic)
-            SettingsAction.OnDismissTopicDropDown -> {}
+            SettingsAction.OnDismissTopicDropDown -> onDismissTopicDropDown()
             is SettingsAction.OnMoodClick -> onMoodClick(action.mood)
             is SettingsAction.OnRemoveTopicClick -> onRemoveTopicClick(action.topic)
-            is SettingsAction.OnSearchTextChange -> {}
+            is SettingsAction.OnSearchTextChange -> onSearchTextChange(action.text)
+        }
+    }
+
+    private fun observeTopicSearchResults() {
+        state
+            .distinctUntilChangedBy { it.searchText }
+            .map { it.searchText }
+            .debounce(300)
+            .flatMapLatest { query ->
+                if (query.isNotBlank()) {
+                    echoDataSource.searchTopics(query)
+                } else emptyFlow()
+            }
+            .onEach { filteredResults ->
+                _state.update {
+                    val filteredNonDefaultResults = filteredResults - it.topics.toSet()
+                    val searchText = it.searchText.trim()
+                    val isNewTopic =
+                        searchText !in filteredNonDefaultResults && searchText !in it.topics
+                                && searchText.isNotBlank()
+                    it.copy(
+                        suggestedTopics = filteredNonDefaultResults,
+                        isTopicSuggestionsVisible = filteredResults.isNotEmpty() || isNewTopic,
+                        showCreateTopicOption = isNewTopic
+                    )
+                }
+            }
+            .flowOn(Dispatchers.Default)
+            .launchIn(viewModelScope)
+    }
+
+    private fun onSearchTextChange(text: String) {
+        _state.update {
+            it.copy(
+                searchText = text
+            )
+        }
+    }
+
+    private fun onDismissTopicDropDown() {
+        _state.update {
+            it.copy(
+                isTopicSuggestionsVisible = false
+            )
+        }
+    }
+
+    private fun onAddButtonClick() {
+        _state.update {
+            it.copy(
+                isTopicTextInputVisible = true
+            )
         }
     }
 
@@ -56,6 +122,13 @@ class SettingsViewModel(
 
     private fun onSelectTopicClick(topic: String) {
         viewModelScope.launch {
+            _state.update {
+                it.copy(
+                    isTopicTextInputVisible = false,
+                    isTopicSuggestionsVisible = false,
+                    searchText = ""
+                )
+            }
             val newDefaultTopics = withContext(Dispatchers.Default) {
                 (state.value.topics + topic).distinct()
             }
